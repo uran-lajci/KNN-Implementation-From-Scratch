e5cd235eb1c1ff5e5cd0da0791e49a5e90a71e9b
uranlajci <117693854+uran-lajci@users.noreply.github.com>
2023-05-01 19:37:27 +0200

    Added the documentation and made improvements to the KNN implementation

--- knn_regression_boston_housing.py
+++ knn_regression_boston_housing.py
@@ -2,6 +2,12 @@ import math
 import operator
 import warnings
 
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
 warnings.filterwarnings("ignore")
 
 
@@ -12,11 +18,25 @@ def euclidean_distance(instance1, instance2, length):
     return math.sqrt(distance)
 
 
-def get_neighbors(training_set, test_instance, k):
+def manhattan_distance(instance1, instance2, length):
+    distance = 0
+    for x in range(length):
+        distance += abs(instance1[x] - instance2[x])
+    return distance
+
+
+def chebyshev_distance(instance1, instance2, length):
+    distance = 0
+    for x in range(length):
+        distance = max(distance, abs(instance1[x] - instance2[x]))
+    return distance
+
+
+def get_neighbors(training_set, test_instance, k, distance_function):
     distances = []
     length = len(test_instance) - 1
     for x in range(len(training_set)):
-        dist = euclidean_distance(test_instance, training_set[x], length)
+        dist = distance_function(test_instance, training_set[x], length)
         distances.append((training_set[x], dist))
     distances.sort(key=operator.itemgetter(1))
     neighbors = []
@@ -30,10 +50,6 @@ def predict_regression(neighbors):
     return prediction
 
 
-import pandas as pd
-from sklearn.datasets import load_boston
-from sklearn.model_selection import train_test_split
-
 # Load the dataset
 boston = load_boston()
 data = pd.DataFrame(boston.data, columns=boston.feature_names)
@@ -44,14 +60,24 @@ train_data, test_data, train_target, test_target = train_test_split(data[boston.
 train_data['MEDV'] = train_target
 test_data['MEDV'] = test_target
 
-# Make predictions on the test data
-predictions = []
-k = 5
-for i in range(len(test_data)):
-    neighbors = get_neighbors(train_data.values, test_data.values[i], k)
-    prediction = predict_regression(neighbors)
-    predictions.append(prediction)
+# Test with different k values and distance functions
+distance_functions = [euclidean_distance, manhattan_distance, chebyshev_distance]
+k_values = [3, 5, 7]
 
-# Compute the root-mean-square-error (RMSE)
-rmse = math.sqrt(sum((test_target - predictions) ** 2) / len(test_target))
-print(f"RMSE: {rmse}")
+for distance_function in distance_functions:
+    for k in k_values:
+        predictions = []
+        for i in range(len(test_data)):
+            neighbors = get_neighbors(train_data.values, test_data.values[i], k, distance_function)
+            prediction = predict_regression(neighbors)
+            predictions.append(prediction)
+
+        # Compute the performance measures
+        rmse = math.sqrt(mean_squared_error(test_target, predictions))
+        mae = mean_absolute_error(test_target, predictions)
+        r2 = r2_score(test_target, predictions)
+
+        print(f"Distance: {distance_function.__name__}, k: {k}")
+        print(f"RMSE: {rmse}")
+        print(f"MAE: {mae}")
+        print(f"R2 Score: {r2}\n")
