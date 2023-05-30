d1ba1207cb56c301e7ecfbcd74ff6efdd1c534f0
uranlajci <117693854+uran-lajci@users.noreply.github.com>
2023-05-30 08:41:43 +0200

    added the knn implementation from scratch

--- knn_implementation_from_scratch.py
+++ knn_implementation_from_scratch.py
@@ -1,47 +1,72 @@
 import math
 import pandas as pd
 
-def euclidean_distance_for_many_dimensions(p_list, q_list, dimension):
+
+def calculate_euclidean_distance(p_coordinates, q_coordinates, num_dimensions):
     total_distance = 0
-    for d in range(dimension):
-        total_distance += pow((p_list[d] - q_list[d]),2)
-    return math.sqrt(total_distance)
+    for dimension in range(num_dimensions):
+        difference_squared = pow((p_coordinates[dimension] - q_coordinates[dimension]), 2)
+        total_distance += difference_squared
+    euclidean_distance = math.sqrt(total_distance)
+    return euclidean_distance
 
 
 def calculate_neighbor_distances(train_dataset, test_dataset):
-    calculated_distances = {}
-    for i in range(len(train_dataset)):
-        calculated_distances[i] = euclidean_distance_for_many_dimensions(train_dataset.iloc[i], test_dataset, train_dataset.shape[1])
-    return calculated_distances
+    neighbor_distances = {}
+    num_dimensions = train_dataset.shape[1]
+    for train_index, train_sample in train_dataset.iterrows():
+        distance = calculate_euclidean_distance(train_sample, test_dataset, num_dimensions)
+        neighbor_distances[train_index] = distance
+    return neighbor_distances
 
 
-def get_neighbors(my_dict, k):
-    sorted_dict = dict(sorted(my_dict.items(), key=lambda item: item[1]))
-    subset = {key: sorted_dict[key] for key in list(sorted_dict)[:k]}
-    return subset
+def get_nearest_neighbors(distances_dict, k):
+    sorted_distances = dict(sorted(distances_dict.items(), key=lambda item: item[1]))
+    nearest_neighbors = {key: sorted_distances[key] for key in list(sorted_distances)[:k]}
+    return nearest_neighbors
 
 
-def predict_regression(selected_elements, k):
-    prediction = sum(selected_elements)/k
+def calculate_regression_prediction(elements, k):
+    prediction = sum(elements) / k
     return prediction
 
+
+def calculate_mean_squared_error(actual_values, predicted_values):
+    squared_errors = []
+    for i in range(len(actual_values)):
+        squared_error = (actual_values[i] - predicted_values[i]) ** 2
+        squared_errors.append(squared_error)
+
+    mean_squared_error = sum(squared_errors) / len(squared_errors)
+    return mean_squared_error
+
+
 if __name__ == "__main__":    
-    df = pd.read_csv("dataset1.csv")
+    df = pd.read_csv("dataset.csv")
 
-    train_dataset = df[["feature1","feature2"]].iloc[0:9]
-    train_dataset_target = df[["target"]].iloc[0:9]
+    # train data
+    train_features = df[["feature1", "feature2"]].iloc[0:9]
+    train_targets = df[["target"]].iloc[0:9]
 
-    test_dataset = df[["feature1","feature2"]].iloc[9]
-    test_dataset_target = df[["target"]].iloc[9]
+    # test data
+    test_features = df[["feature1", "feature2"]].iloc[9]
+    test_target = df[["target"]].iloc[9]
 
-    neighbors = calculate_neighbor_distances(train_dataset, test_dataset)
+    distances = calculate_neighbor_distances(train_features, test_features)
 
     k = 3
-    k_niereset_neighbors = get_neighbors(neighbors, k)
+    nearest_neighbors = get_nearest_neighbors(distances, k)
 
-    selected_elements = []
-    for x in k_niereset_neighbors:
-        selected_elements.append(train_dataset_target.iloc[x])
+    selected_targets = []
+    for neighbor_index in nearest_neighbors:
+        selected_targets.append(train_targets.iloc[neighbor_index])
     
-    knn_prediction = predict_regression(selected_elements, k)
+    knn_prediction = calculate_regression_prediction(selected_targets, k)
     print("Prediction with our code: ", float(knn_prediction))
+
+    actual_value = float(test_target)
+    predicted_value = float(knn_prediction)
+
+    mse = calculate_mean_squared_error([actual_value], [predicted_value])
+    formatted_mse = "{:.2f}".format(mse)
+    print("Mean Squared Error:", formatted_mse)
